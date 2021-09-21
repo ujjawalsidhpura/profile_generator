3df72d57fa0d47e883f22f82cc07492ba3c9b51b
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-21 20:54:12 +0000

    first attemp at readline

---
+++ survey.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name? ', (answer) => {
+  console.log
+    (`Thank you for your valuable feedback, ${answer}`);
+
+  rl.close();
+});
